eb82fdf037e4822342a920c7533a430554695821
LahiruMadushann <lahirumadushandl@gmail.com>
2023-06-13 09:38:42 +0530

    Initial Commit

---
+++ frontend/app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+    return (
+        <View className="bg-gradient-to-r from-cyan-500 to-blue-500 ...">
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
