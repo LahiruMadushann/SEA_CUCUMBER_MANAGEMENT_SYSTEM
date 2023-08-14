d122fc93bbc8b32c265f70de8340f430cbbcb12b
Jeevake Perera <74090690+jeevake@users.noreply.github.com>
2023-08-14 21:03:52 +0530

    Farm Dashboard Commit

--- backend/controller/farmDashboard_controller.js
+++ backend/controller/farmDashboard_controller.js
@@ -35,3 +35,193 @@ exports.getAllFarmersDetails = async (req, res, next) => {
     next(error);
   }
 };
+
+// GET STOCK AND SIZE TRENDS OVER TIME DATA
+exports.getStockAndSizeTrendsOverTime = async (req, res, next) => {
+  try {
+    let stockAndSizeTrends =
+      await farmDashboardService.getStockAndSizeTrendsOverTime();
+
+    if (stockAndSizeTrends) {
+      res.status(200).json({ status: true, data: stockAndSizeTrends });
+    } else {
+      res
+        .status(404)
+        .json({
+          status: false,
+          message: "No stock and size trends data available",
+        });
+    }
+  } catch (error) {
+    console.log(error, "err---->");
+    next(error);
+  }
+};
+
+// GET SURVIVAL RATE ANALYSIS DATA
+exports.getSurvivalRateAnalysis = async (req, res, next) => {
+  try {
+    let survivalRateAnalysis =
+      await farmDashboardService.getSurvivalRateAnalysis();
+
+    if (survivalRateAnalysis) {
+      res.status(200).json({ status: true, data: survivalRateAnalysis });
+    } else {
+      res
+        .status(404)
+        .json({
+          status: false,
+          message: "No survival rate analysis data available",
+        });
+    }
+  } catch (error) {
+    console.log(error, "err---->");
+    next(error);
+  }
+};
+
+// GET HATCHERY BATCH PERFORMANCE DATA
+exports.getHatcheryBatchPerformance = async (req, res, next) => {
+  try {
+    let hatcheryBatchPerformance =
+      await farmDashboardService.getHatcheryBatchPerformance();
+
+    if (hatcheryBatchPerformance) {
+      res.status(200).json({ status: true, data: hatcheryBatchPerformance });
+    } else {
+      res
+        .status(404)
+        .json({
+          status: false,
+          message: "No hatchery batch performance data available",
+        });
+    }
+  } catch (error) {
+    console.log(error, "err---->");
+    next(error);
+  }
+};
+
+// GET HARVEST SIZE DISTRIBUTION DATA
+exports.getHarvestSizeDistribution = async (req, res, next) => {
+  try {
+    let harvestSizeDistribution =
+      await farmDashboardService.getHarvestSizeDistribution();
+
+    if (harvestSizeDistribution) {
+      res.status(200).json({ status: true, data: harvestSizeDistribution });
+    } else {
+      res
+        .status(404)
+        .json({
+          status: false,
+          message: "No harvest size distribution data available",
+        });
+    }
+  } catch (error) {
+    console.log(error, "err---->");
+    next(error);
+  }
+};
+
+// GET COMPARATIVE ANALYSIS BY HATCHERY DATA
+exports.getComparativeAnalysisByHatchery = async (req, res, next) => {
+  try {
+    let comparativeAnalysis =
+      await farmDashboardService.getComparativeAnalysisByHatchery();
+
+    if (comparativeAnalysis) {
+      res.status(200).json({ status: true, data: comparativeAnalysis });
+    } else {
+      res
+        .status(404)
+        .json({
+          status: false,
+          message: "No comparative analysis data available",
+        });
+    }
+  } catch (error) {
+    console.log(error, "err---->");
+    next(error);
+  }
+};
+
+// GET STOCKING DENSITY ANALYSIS DATA
+exports.getStockingDensityAnalysis = async (req, res, next) => {
+  try {
+    let stockingDensityAnalysis =
+      await farmDashboardService.getStockingDensityAnalysis();
+
+    if (stockingDensityAnalysis) {
+      res.status(200).json({ status: true, data: stockingDensityAnalysis });
+    } else {
+      res
+        .status(404)
+        .json({
+          status: false,
+          message: "No stocking density analysis data available",
+        });
+    }
+  } catch (error) {
+    console.log(error, "err---->");
+    next(error);
+  }
+};
+
+// GET DISEASE OCCURRENCE ANALYSIS DATA
+exports.getDiseaseOccurrenceAnalysis = async (req, res, next) => {
+  try {
+    let diseaseOccurrenceAnalysis =
+      await farmDashboardService.getDiseaseOccurrenceAnalysis();
+
+    if (diseaseOccurrenceAnalysis) {
+      res.status(200).json({ status: true, data: diseaseOccurrenceAnalysis });
+    } else {
+      res.status(404).json({
+        status: false,
+        message: "No disease occurrence data available",
+      });
+    }
+  } catch (error) {
+    console.log(error, "err---->");
+    next(error);
+  }
+};
+
+// GET TIME SERIES ANALYSIS DATA
+exports.getTimeSeriesAnalysis = async (req, res, next) => {
+  try {
+    let timeSeriesAnalysis = await farmDashboardService.getTimeSeriesAnalysis();
+
+    if (timeSeriesAnalysis) {
+      res.status(200).json({ status: true, data: timeSeriesAnalysis });
+    } else {
+      res.status(404).json({
+        status: false,
+        message: "No time series analysis data available",
+      });
+    }
+  } catch (error) {
+    console.log(error, "err---->");
+    next(error);
+  }
+};
+
+// GET GEOSPATIAL ANALYSIS DATA
+exports.getGeospatialAnalysis = async (req, res, next) => {
+  try {
+    let geospatialAnalysis = await farmDashboardService.getGeospatialAnalysis();
+
+    if (geospatialAnalysis) {
+      res.status(200).json({ status: true, data: geospatialAnalysis });
+    } else {
+      res.status(404).json({
+        status: false,
+        message: "No geospatial analysis data available",
+      });
+    }
+  } catch (error) {
+    console.log(error, "err---->");
+    next(error);
+  }
+};
--- backend/services/farmDashboard_services.js
+++ backend/services/farmDashboard_services.js
@@ -1,6 +1,7 @@
 /* FARM SECTION DASHBOARD */
 
 const FarmModel = require("../model/farm/aqFarm_model");
+const FarmingModel = require("../model/farm/aqFarmingDetails_model");
 const UserModel = require("../model/user_model");
 
 class FarmDashboardService {
@@ -15,6 +16,156 @@ class FarmDashboardService {
   static async getAllFarmersDetails() {
     const allFarmersDetails = await UserModel.find({ role: "Farmer" });
     return allFarmersDetails;
+  }
+
+  // GET STOCK AND SIZE TRENDS OVER TIME DATA
+  static async getStockAndSizeTrendsOverTime() {
+    const stockAndSizeTrendsOverTime = await FarmingModel.aggregate([
+      {
+        $group: {
+          _id: {
+            year: { $year: "$date" },
+            month: { $month: "$date" },
+          },
+          averageStock: { $avg: "$stock" },
+          averageSize: { $avg: "$size" },
+        },
+      },
+    ]);
+    return stockAndSizeTrendsOverTime;
+  }
+
+  // GET SURVIVAL RATE ANALYSIS DATA
+  static async getSurvivalRateAnalysis() {
+    const survivalRateAnalysis = await FarmingModel.aggregate([
+      {
+        $group: {
+          _id: null,
+          totalSurvival: { $sum: "$survival" },
+          totalStock: { $sum: "$stock" },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          survivalRate: { $divide: ["$totalSurvival", "$totalStock"] },
+        },
+      },
+    ]);
+    return survivalRateAnalysis;
+  }
+  // GET HATCHERY BATCH PERFORMANCE DATA
+  static async getHatcheryBatchPerformance() {
+    const hatcheryBatchPerformance = await FarmingModel.aggregate([
+      {
+        $group: {
+          _id: "$hatcheryBatch",
+          averageSize: { $avg: "$size" },
+          averageSurvival: { $avg: "$survival" },
+          averageDiseaseOccurrence: {
+            $avg: { $cond: { if: "$diseases", then: 1, else: 0 } },
+          },
+        },
+      },
+    ]);
+    return hatcheryBatchPerformance;
+  }
+
+  // GET HARVEST SIZE DISTRIBUTION DATA
+  static async getHarvestSizeDistribution() {
+    const harvestSizeDistribution = await FarmingModel.find({}, "harvest size");
+    return harvestSizeDistribution;
+  }
+
+  // GET COMPARATIVE ANALYSIS BY HATCHERY DATA
+  static async getComparativeAnalysisByHatchery() {
+    const comparativeAnalysisByHatchery = await FarmingModel.aggregate([
+      {
+        $group: {
+          _id: "$hatchery",
+          averageSurvival: { $avg: "$survival" },
+          averageHarvestSize: { $avg: "$harvest" },
+          averageDiseaseOccurrence: {
+            $avg: { $cond: { if: "$diseases", then: 1, else: 0 } },
+          },
+        },
+      },
+    ]);
+    return comparativeAnalysisByHatchery;
+  }
+
+  // GET STOCKING DENSITY ANALYSIS DATA
+  static async getStockingDensityAnalysis() {
+    const stockingDensityAnalysis = await FarmingModel.aggregate([
+      {
+        $group: {
+          _id: "$stockingDensity",
+          averageSize: { $avg: "$size" },
+          averageSurvival: { $avg: "$survival" },
+          averageDiseaseOccurrence: {
+            $avg: { $cond: { if: "$diseases", then: 1, else: 0 } },
+          },
+        },
+      },
+    ]);
+    return stockingDensityAnalysis;
+  }
+
+  // GET DISEASE OCCURRENCE ANALYSIS DATA
+  static async getDiseaseOccurrenceAnalysis() {
+    const diseaseOccurrenceAnalysis = await FarmingModel.aggregate([
+      {
+        $match: {
+          diseases: { $exists: true },
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          totalDiseases: { $sum: 1 },
+          averageSurvival: { $avg: "$survival" },
+          averageStock: { $avg: "$stock" },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          totalDiseases: 1,
+          averageSurvival: 1,
+          averageStock: 1,
+        },
+      },
+    ]);
+    return diseaseOccurrenceAnalysis;
+  }
+
+  // GET TIME SERIES ANALYSIS DATA
+  static async getTimeSeriesAnalysis() {
+    const timeSeriesAnalysis = await FarmingModel.aggregate([
+      {
+        $group: {
+          _id: {
+            year: { $year: "$date" },
+            month: { $month: "$date" },
+          },
+          averageSurvival: { $avg: "$survival" },
+          averageSize: { $avg: "$size" },
+        },
+      },
+      {
+        $sort: {
+          "_id.year": 1,
+          "_id.month": 1,
+        },
+      },
+    ]);
+    return timeSeriesAnalysis;
+  }
+
+  // GET GEOSPATIAL ANALYSIS DATA
+  static async getGeospatialAnalysis() {
+    const geospatialAnalysis = await FarmingModel.find().populate("farmId");
+    return geospatialAnalysis;
   }
 }
 
