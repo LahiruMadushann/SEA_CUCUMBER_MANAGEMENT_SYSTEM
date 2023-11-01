3eb711ffc38d3d6a01b71f5739218c5b1321acb6
Jeevake Perera <74090690+jeevake@users.noreply.github.com>
2023-11-01 09:26:50 +0530

    Port Changed Commit 3000

--- backend/index.js
+++ backend/index.js
@@ -7,27 +7,26 @@ const userModel = require("./model/user_model");
 // const aqFarmingDetailsModel = require("./model/farm/aqFarmingDetails_model");
 
 connection();
-const port = 5001;
+const port = 3000;
 
 // app.get("/", (req, res) => {
 //   res.send("Hello World!!!!");
 // });
 
-app.get('/profile', (req, res) => {
-  userModel.findById(req.user.id)
-    .select('-password') // Exclude the 'password' field from the response
-    .then(user => {
+app.get("/profile", (req, res) => {
+  userModel
+    .findById(req.user.id)
+    .select("-password") // Exclude the 'password' field from the response
+    .then((user) => {
       if (user) {
         res.json(user);
-        console.log("Data hrid blnn",user);
+        console.log("Data hrid blnn", user);
       } else {
         res.status(404).json({ error: "User not found" });
       }
     })
-    .catch(err => res.status(500).json({ error: err.message }));
+    .catch((err) => res.status(500).json({ error: err.message }));
 });
-
-
 
 app.listen(port, () => {
   console.log(`Server Listening on Port http://localhost:${port}`);
